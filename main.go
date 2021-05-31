97fb0be93f06da30734f7d195be60347a479aa2b
clshu <clshu2020@gmail.com>
2021-05-31 02:11:16 -0700

    Change code to exec the program with env in the dotenv file. But it fork the process and no stdout. I'll add the code in server.go. Save the file for the future refrence.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 
@@ -9,21 +10,48 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Missing file name Argument")
+	var name string
+	var args []string
+
+	if len(os.Args) < 3 {
+		fmt.Println("setenv dotenvfile [go|fresh]")
 		os.Exit(1)
 	}
-	fileName := os.Args[1]
+	dotenvfile := os.Args[1]
 
-	envMap, err := godotenv.Read(fileName)
+	envMap, err := godotenv.Read(dotenvfile)
 	if err != nil {
-		fmt.Printf("Reading file %v failed. %v", fileName, err.Error())
+		fmt.Printf("Reading file %v failed. %v", dotenvfile, err.Error())
 		os.Exit(2)
-	}	
-	
+	}
+
+	if envMap["NODE_ENV"] != "development" && envMap["NODE_ENV"] != "test" {
+		fmt.Println("It is not development or test environment")
+		os.Exit(0)
+	}
+	switch os.Args[2] {
+	case "go":
+		name = "go run"
+		args = []string{"server.go"}
+		break
+	case "fresh":
+		name = "fresh"
+		args = []string{}
+		break
+	default:
+		fmt.Println("setenv dotenvfile [go|fresh]")
+		os.Exit(3)
+	}
+
+	cmd := exec.Command(name, args...)
+
 	for key, value := range envMap {
-		str := "export " + key + "=" + value
-		cmd := exec.Command(str)
-		cmd.Run()
-	}	
+		str := key + "=" + value
+		cmd.Env = append(os.Environ(), str)
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
 }
